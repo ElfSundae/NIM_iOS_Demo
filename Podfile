1f028d290816d4546054fa5fcdb23ee7f4a091bb
Elf Sundae <elf.sundae@gmail.com>
2019-12-14 17:28:39 +0800

    Update update-source.sh and project.yml

--- Podfile
+++ Podfile
@@ -1,10 +1,6 @@
-DEPLOYMENT_VERSION = '9.0'.freeze
-DEPLOYMENT_TARGET_KEY = 'IPHONEOS_DEPLOYMENT_TARGET'.freeze
-PODS_MIN_DEPLOYMENT_VERSION = '9.0'.freeze
+platform :ios, '9.0'
 
-platform :ios, DEPLOYMENT_VERSION
-
-target 'NIMDemo' do
+target 'NIM' do
     pod 'NIMKit/Full_Free', '~> 2.12'
     pod 'Toast', '~> 3.0'
 
@@ -16,22 +12,4 @@ target 'NIMDemo' do
     pod 'SVProgressHUD', '~> 2.1'
     pod 'Fabric'
     pod 'Crashlytics'
-end
-
-# https://github.com/CocoaPods/CocoaPods/issues/7314#issuecomment-489453484
-def fix_deployment_targets(installer)
-  installer.generated_projects.each do |project|
-    project.targets.each do |target|
-      target.build_configuration_list.build_configurations.each do |config|
-        if config.build_settings[DEPLOYMENT_TARGET_KEY].to_f < PODS_MIN_DEPLOYMENT_VERSION.to_f
-          config.build_settings[DEPLOYMENT_TARGET_KEY] = PODS_MIN_DEPLOYMENT_VERSION
-          # puts "Successfully set #{DEPLOYMENT_TARGET_KEY} of target #{target.name} for config #{config.display_name} to #{PODS_MIN_DEPLOYMENT_VERSION}"
-        end
-      end
-    end
-  end
-end
-
-post_install do |installer|
-  fix_deployment_targets installer
 end
--- Scripts/update-source.sh
+++ Scripts/update-source.sh
@@ -3,46 +3,63 @@ set -euo pipefail
 
 URL='https://yx-web-nosdn.netease.im/package/1575456096/NIM_iOS_Demo_v7.0.3.zip?download=NIM_iOS_Demo_v7.0.3.zip'
 
-# @args: url path Enterprise|Professional
+WORKING=working
+
 download()
 {
-    if [[ -f $2 ]] && ! check_version_in_zip $2 $3; then
-        rm -f $2
+    destFile="$WORKING/$(basename "${1%\?*}")"
+    tempFile="$destFile.tmp"
+
+    if [[ ! -f "$destFile" ]]; then
+        wget "$1" -c -O "$tempFile" && mv "$tempFile" "$destFile" \
+            || rm -f "$tempFile"
     fi
 
-    if [[ ! -f $2 ]]; then
-        wget $1 -c -O $2.tmp && mv $2.tmp $2 || rm -f $2.tmp
-
-        if [[ ! -f $2 ]]; then
-            exit 404
-        fi
-    fi
-
-    if ! check_version_in_zip $2 $3; then
-        rootInZip=$(root_dir_in_zip $2)
-        echo "Error: SDK version in the zip file ($rootInZip) is not equal to $SDK_VERSION"
-        exit 1
+    if [[ -f "$destFile" ]]; then
+        echo "$destFile"
     fi
 }
 
-Remove:
-NIMDemo/Supporting%20Files/NIMKitEmoticon.bundle
-NIMDemo/Supporting%20Files/NIMKitResource.bundle
-NIMDemo/Vendors/NIMAVChat
-NIMDemo/Vendors/NIMSDK
+cd "$(dirname "$0")/.."
+mkdir -p "$WORKING"
 
-mv:
-Settings.bundle to NIMDemo/Supporting Files
+downloadedFile=$(download "$URL")
+[ -z "$downloadedFile" ] && exit 4
 
-Run xcodegen, remove xcworkspace, pod update,
+srcRoot=${downloadedFile%.zip}
+rm -rf "$srcRoot"
+unzip -q "$downloadedFile" -d "$srcRoot"
 
-Show diff patch:
+rsync -a --delete "$srcRoot/NIMDemo/NIMDemo" .
+rsync -a --delete "$srcRoot/NIMDemo/NotificationScene" .
+rsync -a --delete "$srcRoot/NIMDemo/NotificationService" .
+rsync -a --delete "$srcRoot/NIMDemo/Settings.bundle" "NIMDemo/Supporting Files"
 
-cd official
-xcodeproj sort ~/Downloads/NIM_iOS_Demo_v7.0.3/NIMDemo/NIM.xcodeproj
-xcodeproj show ~/Downloads/NIM_iOS_Demo_v7.0.3/NIMDemo/NIM.xcodeproj > ~/Desktop/official
+rm -rf "NIMDemo/Supporting Files/NIMKitEmoticon.bundle"
+rm -rf "NIMDemo/Supporting Files/NIMKitResource.bundle"
+rm -rf "NIMDemo/Vendors/NIMAVChat"
+rm -rf "NIMDemo/Vendors/NIMSDK"
 
-xcodeproj sort ~/working/NIM_iOS_Demo/NIMDemo.xcodeproj
-xcodeproj show ~/working/NIM_iOS_Demo/NIMDemo.xcodeproj > ~/Desktop/my
+echo "Updated official source code."
 
-diff -u ~/Desktop/official ~/Desktop/my > ~/Desktop/diff.patch
+if ! [[ -x "$(command -v xcodegen)" ]]; then
+    echo 'Error: XcodeGen is not installed, see https://github.com/yonaskolb/XcodeGen'
+    echo 'You may install XcodeGen by running `brew install xcodegen`'
+    exit 1
+fi
+
+xcodegen
+
+pod update
+
+# TODO: Compare generated project with the official project
+# Show diff patch:
+
+# cd official
+# xcodeproj sort ~/Downloads/NIM_iOS_Demo_v7.0.3/NIMDemo/NIM.xcodeproj
+# xcodeproj show ~/Downloads/NIM_iOS_Demo_v7.0.3/NIMDemo/NIM.xcodeproj > ~/Desktop/official
+
+# xcodeproj sort ~/working/NIM_iOS_Demo/NIMDemo.xcodeproj
+# xcodeproj show ~/working/NIM_iOS_Demo/NIMDemo.xcodeproj > ~/Desktop/my
+
+# diff -u ~/Desktop/official ~/Desktop/my > ~/Desktop/diff.patch
