4fec508b6e809b8ccfed296329a8f33f844f90e4
Elf Sundae <elf.sundae@gmail.com>
2020-09-13 22:54:56 +0800

    NIM_iOS_Demo_v7.9.1

--- Scripts/update-source.sh
+++ Scripts/update-source.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Update source code from Netease official project:
 # https://yunxin.163.com/im-sdk-demo
-URL='https://yx-web-nosdn.netease.im/package/1598517074/NIM_iOS_Demo_v7.9.0.zip?download=NIM_iOS_Demo_v7.9.0.zip'
+URL='https://yx-web-nosdn.netease.im/package/1599806294/NIM_iOS_Demo_v7.9.1.zip?download=NIM_iOS_Demo_v7.9.1.zip'
 
 WORKING=working
 
